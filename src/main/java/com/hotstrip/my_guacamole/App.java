397ea296e29f655a46aa899043ac0d25997524e7
Eleven <idiotalex@163.com>
2020-04-26 18:27:58 +0800

    添加依赖

---
+++ src/main/java/com/hotstrip/my_guacamole/App.java
@@ -0,0 +1,16 @@
+package com.hotstrip.my_guacamole;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    private static Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        logger.info("application start...");
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ src/main/java/com/hotstrip/my_guacamole/HttpTunnelServlet.java
@@ -0,0 +1,5 @@
+package com.hotstrip.my_guacamole;
+
+public class HttpTunnelServlet {
+
+}
