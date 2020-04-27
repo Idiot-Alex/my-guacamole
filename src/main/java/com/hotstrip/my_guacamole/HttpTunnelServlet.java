104a5daf75180b1d2398239bd2b21b1612207964
Eleven <idiotalex@163.com>
2020-04-27 15:51:24 +0800

    更新代码

--- src/main/java/com/hotstrip/my_guacamole/HttpTunnelServlet.java
+++ src/main/java/com/hotstrip/my_guacamole/HttpTunnelServlet.java
@@ -1,5 +1,36 @@
 package com.hotstrip.my_guacamole;
 
-public class HttpTunnelServlet {
+import org.apache.guacamole.GuacamoleException;
+import org.apache.guacamole.net.GuacamoleSocket;
+import org.apache.guacamole.net.GuacamoleTunnel;
+import org.apache.guacamole.net.InetGuacamoleSocket;
+import org.apache.guacamole.net.SimpleGuacamoleTunnel;
+import org.apache.guacamole.protocol.ConfiguredGuacamoleSocket;
+import org.apache.guacamole.protocol.GuacamoleConfiguration;
+import org.apache.guacamole.servlet.GuacamoleHTTPTunnelServlet;
 
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+
+@WebServlet(urlPatterns = "/tunnel")
+public class HttpTunnelServlet extends GuacamoleHTTPTunnelServlet {
+
+    @Override
+    protected GuacamoleTunnel doConnect(HttpServletRequest httpServletRequest) throws GuacamoleException {
+        String hostName = "192.168.12.50";
+        int port = 4823;
+
+        // Guacamole 配置
+        GuacamoleConfiguration configuration = new GuacamoleConfiguration();
+        configuration.setProtocol("rdp");
+        configuration.setParameter("hostname", "192.168.12.69");
+        configuration.setParameter("port", "3389");
+        configuration.setParameter("username", "administrator");
+        configuration.setParameter("password", "123456");
+        configuration.setParameter("ignore-cert", "true");
+
+        GuacamoleSocket socket = new ConfiguredGuacamoleSocket(new InetGuacamoleSocket(hostName, port), configuration);
+        GuacamoleTunnel tunnel = new SimpleGuacamoleTunnel(socket);
+        return tunnel;
+    }
 }
